e542abcda58f1e9d8228594784ca85a6311d2cff
Leo-1636 <leoworkroom1@gmail.com>
2026-02-23 10:37:20 +0800

    Update Dataset

--- scripts/config/settings.py
+++ scripts/config/settings.py
@@ -10,16 +10,14 @@ class ModelConfig:
     yolo26 = "yolo26l.pt"
     yolo11 = "yolo11l.pt"
 
-
 class TrainConfig:
     epochs = 50
     batch_size = 16
     image_size = 640
 
 class DatasetConfig:
-    path: Path = Path("datasets")
+    path: Path = Path(__file__).parents[2] / "datasets"
     image_size: tuple[int, int] = (640, 640)
-
 
 class WandbConfig:
     api_key: str | None = os.getenv("WANDB_API_KEY")
